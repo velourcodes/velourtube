38b404f09bd3d34c9d243f92ad253a448d988ea2
velourcodes <mohammadaffansiddiqi98@gmail.com>
2026-02-14 17:31:56 +0530

    Added dynamic path to multer middleware for production

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,11 +1,11 @@
 import multer from "multer";
-
+import path from "path";
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public/temp");
+        const folderPath = path.join(process.cwd(), "public", "temp");
+        cb(null, folderPath);
     },
     filename: function (req, file, cb) {
-        // const uniquename = Date.now() + "-" + file.originalname;
         cb(null, file.originalname);
     },
 });
